45f9fee2321858483b9ddeaa2fc88130fbeae9ed
73pp31in <46528783+73pp31in@users.noreply.github.com>
2019-07-25 12:41:51 +0530

    Add files via upload

---
+++ HelloYou.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#Ask name
+name = raw_input("What is your name? ")
+
+#Ask age
+age = raw_input("What is your age? ")
+
+#Ask location
+loc = raw_input("Where do you live? ")
+
+#Ask hobbies
+hob = raw_input("What do you like to do? ")
+
+print ("Your name is " + name + ", you are " + age + " years old, you live in " + loc + " and you like " + hob + ".")
